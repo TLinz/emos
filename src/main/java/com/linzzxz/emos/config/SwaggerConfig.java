4f2cb1e6e6cfca1c8f74af6dc108b44825c5a65d
TLinz <linz1114@163.com>
2023-10-27 01:18:53 +0200

    Swagger2迁移至openapi

--- src/main/java/com/linzzxz/emos/config/SwaggerConfig.java
+++ src/main/java/com/linzzxz/emos/config/SwaggerConfig.java
@@ -1,62 +1,26 @@
 package com.linzzxz.emos.config;
 
-import io.swagger.annotations.ApiOperation;
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.ApiSelectorBuilder;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
-@EnableSwagger2
+@OpenAPIDefinition(
+        info = @Info(
+                title = "emos-api",
+                description = "EMOS",
+                version = "1.0"
+        )
+)
+@SecurityScheme(
+        // 请求头中Token字段
+        name = "Token",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 public class SwaggerConfig {
-    @Bean
-    public Docket createRestApi() {
-        Docket docket = new Docket(DocumentationType.SWAGGER_2);
-        // 设置Swagger页面基本信息
-        ApiInfoBuilder builder = new ApiInfoBuilder();
-        builder.title("EMOS");
-        ApiInfo info = builder.build();
-        docket.apiInfo(info);
 
-        ApiSelectorBuilder selectorBuilder = docket.select();
-        // 为Swagger页面添加所有包下的所有类
-        selectorBuilder.paths(PathSelectors.any());
-        // 要求添加到Swagger页面的方法必须标注ApiOperation注解
-        selectorBuilder.apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class));
-        selectorBuilder.build();
-        docket = selectorBuilder.build();
-
-        // 开启Swagger对jwt的支持，当用户使用Swagger调用受jwt保护的方法，必须提交令牌
-        ApiKey apiKey = new ApiKey("token", "token", "header");
-        List<ApiKey> apiKeyList = new ArrayList<>();
-        apiKeyList.add(apiKey);
-        docket.securitySchemes(apiKeyList);
-
-        // 设定令牌作用域，如果用户jwt认证通过，则在Swagger中全局有效
-        AuthorizationScope scope = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] scopes = {scope};
-        SecurityReference reference = new SecurityReference("token", scopes);
-        List refList = new ArrayList();
-        refList.add(reference);
-        SecurityContext context = SecurityContext.builder().securityReferences(refList).build();
-        List cxtList = new ArrayList();
-        cxtList.add(context);
-        docket.securityContexts(cxtList);
-
-        return docket;
-    }
 }
--- src/main/java/com/linzzxz/emos/controller/TestController.java
+++ src/main/java/com/linzzxz/emos/controller/TestController.java
@@ -1,18 +1,18 @@
 package com.linzzxz.emos.controller;
 
 import com.linzzxz.emos.common.util.R;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
-@Api("test web api")
+@Tag(name = "test web api")
 public class TestController {
     @GetMapping("sayHello")
-    @ApiOperation("simple test method")
+    @Operation(summary = "simple test method")
     public R sayhello() {
         return R.ok().put("msg", "Hello from the other side.");
     }
