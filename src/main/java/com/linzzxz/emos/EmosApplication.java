15fc0d95fa9c825640293a62717a284192d977d1
TLinz <linz1114@163.com>
2023-10-26 18:57:27 +0200

    修复启动数据源相关错误

--- src/main/java/com/linzzxz/emos/EmosApplication.java
+++ src/main/java/com/linzzxz/emos/EmosApplication.java
@@ -2,8 +2,11 @@ package com.linzzxz.emos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// 不使用autoconifg读取数据源配置，若不添加exclude则发生报错：
+// Failed to determine a suitable driver class
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class EmosApplication {
 
     public static void main(String[] args) {
