607850787c6f9a5b289337130c065bfed28a81a9
TLinz <linz1114@163.com>
2023-10-27 01:44:35 +0200

    配置后端验证

--- src/main/java/com/linzzxz/emos/controller/TestController.java
+++ src/main/java/com/linzzxz/emos/controller/TestController.java
@@ -1,19 +1,19 @@
 package com.linzzxz.emos.controller;
 
 import com.linzzxz.emos.common.util.R;
+import com.linzzxz.emos.controller.form.TestSayHelloForm;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/test")
 @Tag(name = "test web api")
 public class TestController {
-    @GetMapping("sayHello")
+    @PostMapping("sayHello")
     @Operation(summary = "simple test method")
-    public R sayhello() {
-        return R.ok().put("msg", "Hello from the other side.");
+    public R sayhello(@Valid @RequestBody TestSayHelloForm form) {
+        return R.ok().put("message", "Hello from " + form.getName());
     }
 }
---
+++ src/main/java/com/linzzxz/emos/controller/form/TestSayHelloForm.java
@@ -0,0 +1,16 @@
+package com.linzzxz.emos.controller.form;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+@Schema(description = "检验用户姓名")
+public class TestSayHelloForm {
+    @NotBlank(message = "姓名不能为空")
+    // 规定姓名必须为数量为2-15之间的简体汉字
+    @Pattern(regexp = "^[\\u4e00-\\u9fa5]{2,15}$")
+    @Schema(description = "姓名")
+    private String name;
+}
