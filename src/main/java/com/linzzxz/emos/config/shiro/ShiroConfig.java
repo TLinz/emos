649f8d1c3322efd53f6936563c1b22a573872f2f
TLinz <linz1114@163.com>
2023-10-29 20:35:13 +0100

    fixed bugs in ShiroConfg

--- src/main/java/com/linzzxz/emos/config/shiro/ShiroConfig.java
+++ src/main/java/com/linzzxz/emos/config/shiro/ShiroConfig.java
@@ -1,11 +1,11 @@
 package com.linzzxz.emos.config.shiro;
 
 import jakarta.servlet.Filter;
-import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,7 +17,7 @@ import java.util.Map;
 public class ShiroConfig {
     @Bean("securityManager")
     public SecurityManager securityManager(OAuth2Realm oAuth2Realm) {
-        DefaultSecurityManager securityManager = new DefaultSecurityManager();
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
         securityManager.setRealm(oAuth2Realm);
         securityManager.setRememberMeManager(null);
         return securityManager;
