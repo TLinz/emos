58e7cf58ad8887cd6d798aba90b480e749df05ce
TLinz <linz1114@163.com>
2023-10-29 07:05:50 +0100

    创建OAuth2Token类和OAuth2Realm类（未实现认证授权逻辑）

---
+++ src/main/java/com/linzzxz/emos/config/shiro/OAuth2Token.java
@@ -0,0 +1,21 @@
+package com.linzzxz.emos.config.shiro;
+
+import org.apache.shiro.authc.AuthenticationToken;
+
+public class OAuth2Token implements AuthenticationToken {
+    private String token;
+
+    public OAuth2Token(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return null;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+}
